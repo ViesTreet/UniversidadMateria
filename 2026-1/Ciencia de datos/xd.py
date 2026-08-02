4638a1123db10f8b5677cf5efa8d7fb765d4ba6f
ViesTreet <javierulloafierro@gmail.com>
2026-08-02 00:42:45 -0400

    proximo semestre2

--- 2026-1/Ciencia de datos/xd.py
+++
@@ -1,9 +0,0 @@
-
-
-with open("Ciencia de datos.md","a") as file:
-    for i in range(1,57):
-        if (i<10):
-            dato=f"0{i}"
-        else:
-            dato=str(i)
-        file.write(f"![[W1_13__Deployment_page-00{dato}.jpg]]\n\n")
